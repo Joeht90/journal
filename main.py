04b61886fb9596a75d7147290e8826a1ee70862d
joeht90 <joeht90@icloud.com>
2025-04-20 20:47:24 -0700

    Initial testing to create an area to edit multiple lines of text

---
+++ main.py
@@ -0,0 +1,10 @@
+import sys
+
+# class Entry:
+    
+    
+#     def __init__(self, title, body):
+#         self.title = title
+#         self.body = body
+# This will allow me to add multiple lines of text but it doesnt let me edit previous lines
+# text = sys.stdin.readlines()
